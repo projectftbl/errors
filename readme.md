2d3230e7f4e984234615f94520e49e8bbb4bcbe0
Johnny Hall <johnny@recipher.co.uk>
2015-12-23 00:19:34 +0000

    Fix tabs

--- readme.md
+++ readme.md
@@ -3,10 +3,10 @@
 Server errors.
 
 ```javascript
-throw new DeclinedError();				// 423
-throw new DuplicateError();				// 422
+throw new DeclinedError();        // 423
+throw new DuplicateError();       // 422
 throw new NotAuthorizedError();   // 403
-throw new NotFoundError(); 				// 404
+throw new NotFoundError();        // 404
 throw new UnauthenticatedError(); // 401
-throw new ValidationError();  		// 422 
+throw new ValidationError();      // 422 
 ```
--- test/index.js
+++ test/index.js
@@ -2,76 +2,76 @@ var should = require('chai').should()
   , errors = require('../');
 
 describe('errors', function() {
-	describe('declined', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.DeclinedError('declined');
-			}).should.throw(errors.DeclinedError, /declined/);
-		});
+  describe('declined', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.DeclinedError('declined');
+      }).should.throw(errors.DeclinedError, /declined/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.DeclinedError().status.should.equal(423);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.DeclinedError().status.should.equal(423);
+    });
+  });
 
-	describe('duplicate', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.DuplicateError('duplicate');
-			}).should.throw(errors.DuplicateError, /duplicate/);
-		});
+  describe('duplicate', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.DuplicateError('duplicate');
+      }).should.throw(errors.DuplicateError, /duplicate/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.DuplicateError().status.should.equal(422);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.DuplicateError().status.should.equal(422);
+    });
+  });
 
-	describe('validation', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.ValidationError('validation');
-			}).should.throw(errors.ValidationError, /validation/);
-		});
+  describe('validation', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.ValidationError('validation');
+      }).should.throw(errors.ValidationError, /validation/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.ValidationError().status.should.equal(422);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.ValidationError().status.should.equal(422);
+    });
+  });
 
-	describe('unauthenticated', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.UnauthenticatedError('unauthenticated');
-			}).should.throw(errors.UnauthenticatedError, /unauthenticated/);
-		});
+  describe('unauthenticated', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.UnauthenticatedError('unauthenticated');
+      }).should.throw(errors.UnauthenticatedError, /unauthenticated/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.UnauthenticatedError().status.should.equal(401);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.UnauthenticatedError().status.should.equal(401);
+    });
+  });
 
-	describe('not found', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.NotFoundError('not found');
-			}).should.throw(errors.NotFoundError, /not found/);
-		});
+  describe('not found', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.NotFoundError('not found');
+      }).should.throw(errors.NotFoundError, /not found/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.NotFoundError().status.should.equal(404);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.NotFoundError().status.should.equal(404);
+    });
+  });
 
-	describe('not authorized', function() {
-		it('should throw error', function() {
-			(function() {
-				throw new errors.NotAuthorizedError('not authorized');
-			}).should.throw(errors.NotAuthorizedError, /not authorized/);
-		});
+  describe('not authorized', function() {
+    it('should throw error', function() {
+      (function() {
+        throw new errors.NotAuthorizedError('not authorized');
+      }).should.throw(errors.NotAuthorizedError, /not authorized/);
+    });
 
-		it('should have correct status code', function() {
-			new errors.NotAuthorizedError().status.should.equal(403);
-		});
-	});
+    it('should have correct status code', function() {
+      new errors.NotAuthorizedError().status.should.equal(403);
+    });
+  });
 
 });
